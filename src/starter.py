8db805b4d23954ac1f08a07ec37870f77ef9ae85
CatLearned <vovanvinni@gmail.com>
2021-01-04 17:18:00 +0300

    Initial commit

---
+++ src/starter.py
@@ -0,0 +1,11 @@
+import os
+import src.demonstr as ssvep
+
+
+def main():
+# Define some variables
+    record_duration = 5000
+    ssvep.present(duration=record_duration)
+
+if __name__ == '__main__':
+    main()
